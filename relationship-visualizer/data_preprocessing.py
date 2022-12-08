1fe21407954c06946a427e5c2b1477fe335f1703
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2022-12-08 10:28:21 +0100

    changes for data relationship splits

---
+++ relationship-visualizer/data_preprocessing.py
@@ -0,0 +1,126 @@
+import sys
+from typing import Any, List, TextIO, Tuple, Type, Union
+import numpy as np
+import click
+import torch
+import matplotlib.pyplot as plt
+import transformers as transf
+from pykeen.datasets import get_dataset
+from nltk.stem import PorterStemmer
+from keras.utils import pad_sequences
+import seaborn as sns
+from scipy.spatial.distance import pdist,squareform
+from sklearn.decomposition import PCA
+import requests
+import time
+import json
+import pandas as pd
+import glob
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from functools import lru_cache
+
+def get_data_frame(dataset_name="fb15k237"):
+    dataset = get_dataset(dataset="fb15k237")
+    # get a list of all the .txt files in the current directory
+    txt_files = glob.glob(f'{dataset_name}/*.txt')
+
+    dataframe = list()
+    # concatenate the contents of all the .txt files into a single string
+    txt = 'head\trelationship\ttail\n'
+    for i, file in enumerate(txt_files):
+        df = pd.read_csv(file, sep='\t')
+        dataframe.append(df)
+    df = pd.concat([dataframe[0], dataframe[1], dataframe[2]], axis=0)
+    
+    # create a DataFrame from the list of lines
+    # df = pd.DataFrame(lines, columns=['head', 'relationship', 'tail'])
+
+    # print the resulting DataFrame
+    return df, dataset.entity_to_id
+
+def cluster_relationship(df):
+    unique_values = df['relationship'].unique()
+
+    print(len(unique_values))
+    # create a TfidfVectorizer to represent each value as a vector
+    vectorizer = TfidfVectorizer()
+    vectors = vectorizer.fit_transform(unique_values)
+
+    # create a KMeans model with K=3 clusters
+    kmeans = KMeans(n_clusters=3)
+
+    # fit the model to the vectors
+    kmeans.fit(vectors)
+
+    # predict the cluster for each vector
+    clusters = kmeans.predict(vectors)
+    # print the cluster for each value
+    cluster_frame = [[], [], []]
+    for value, cluster in zip(unique_values, clusters):
+        print(f'{value}: cluster {cluster}')
+        cluster_frame[cluster].append(value)
+    return cluster_frame
+
+
+@lru_cache(maxsize=None)
+def fetch_wikidata_value_for_id(id):
+    try:
+        word = requests.get(url=f"https://www.wikidata.org/wiki/Special:EntityData/Q{id}.json", timeout=5).json()
+    except Exception as e:
+        print(f"Could not get the details for {id}. Skipping this relationship")
+        return
+    entity = word['entities']
+    if entity.get(f'Q{id}'):
+        sentence_desc=entity[f'Q{id}']['descriptions']
+        if sentence_desc.get('en'):
+            sentence = sentence_desc['en']['value']
+            return sentence
+    else:
+        print(f"Skipping relationship = {id} because no english description is available")
+        return
+
+def get_triples_as_value(values, entity_to_id):
+    rows = list()
+    for row in values.iterrows():
+        head = row[1]['head']
+        tail=row[1]['tail']
+        # Get the head and tail corresponding to this
+        # Use pykeen to find the corresponding entity to entity id
+        head_id=entity_to_id[head]
+        tail_id=entity_to_id[tail]
+        # Make a request to get the value
+        head_val = fetch_wikidata_value_for_id(head_id)
+        tail_val = fetch_wikidata_value_for_id(tail_id)
+        if head_val == None or tail_val == None:
+            print("Skipping the HEAD and TAIL for the row")
+            continue
+        rows.append([head_val, row[1]['relationship'], tail_val])
+    return rows
+
+
+
+def get_triples_from_cluster(cluster_frame, df, entity_to_id):
+    train_test_dev = [list(), list(), list()]
+    # For each cluster get the triple from the dataframe
+    for i, cluster in enumerate(cluster_frame):
+        # For each of the cluster, get the triple for the corresponding relation
+        for relation in cluster:
+            train_test_dev[i].extend(get_triples_as_value(df.loc[df['relationship'] == relation], entity_to_id))
+
+    print(train_test_dev)
+    # Save to CSV
+    df_train = pd.DataFrame(np.asarray(train_test_dev[0]), columns = ['head','relationship','tail'])
+    df_test = pd.DataFrame(np.asarray(train_test_dev[1]), columns = ['head','relationship','tail'])
+    df_val = pd.DataFrame(np.asarray(train_test_dev[2]), columns = ['head','relationship','tail'])
+    df_train.to_csv('train.csv', index=False)
+    df_test.to_csv('test.csv', index=False)
+    df_val.to_csv('validation.csv', index=False)
+
+
+df, dataset_entity_id = get_data_frame("~/kg-super-engine/fb15k237")
+
+cluster_frame = cluster_relationship(df)
+
+triples_from_cluster = get_triples_from_cluster(cluster_frame, df, dataset_entity_id)
--- relationship-visualizer/view-relationship.py
+++ relationship-visualizer/view-relationship.py
@@ -13,14 +13,39 @@ from scipy.spatial.distance import pdist,squareform
 from sklearn.decomposition import PCA
 import requests
 import time
+import json
 
 ps = PorterStemmer()
 
 
-def generate_relationship_sim_matrix(dataset_name, similarity_type="jaccard"):
+def build_train_test_dev(array_similarity_matrix, threshold):
+    set_train = set()
+    set_dev = set()
+    set_test = set()
+    for i, row in enumerate(array_similarity_matrix):
+        for j, row_val in enumerate(row):
+            if row_val > threshold:
+                set_train.add(i)
+                if len(set_dev) < 100:
+                    set_dev.add(j)
+                else:
+                    set_test.add(j)
+    # Get the intersection
+    # Clean up the train, test, dev
+    return set_train, set_test, set_dev
+
+def generate_sentence_embeddings():
+    # Use SBERT to generate the embeddings
+    # Visualize if possible
+    # Look at PCA to project these embeddings and split the dataset into Train, Test, Dev
+    return
+
+
+def generate_relationship_sim_matrix(dataset_name, similarity_type="cosine"):
     print("Getting dataset")
     dataset = get_dataset(dataset=dataset_name)
     relationship_array = list()
+    relationship_label = list()
     if dataset_name=="Wikidata5M":
         for value in dataset.relation_to_id.keys():
             print(f"LOADing for {value}")
@@ -40,23 +65,41 @@ def generate_relationship_sim_matrix(dataset_name, similarity_type="jaccard"):
             relationship_array.append(sentence.split())
     #         Slow down the request because of rate limiter
             time.sleep(1)
+        relationshio_obj = {"obj":relationship_array}
+        with open("wikidata5m.json", 'w') as file:
+            json.dump(relationshio_obj, file)
     else:
+        
+        # Check for the triple in train, dev, test and add it to the array
+        all_triples = [dataset.training.label_triples(dataset.training.mapped_triples), dataset.validation.label_triples(dataset.validation.mapped_triples), dataset.testing.label_triples(dataset.testing.mapped_triples)]
+        # print(dataset.training.relation_id_to_label)
+        # :TODO flatten this thing
+        all_entity_ids = [dataset.training.entity_to_id, dataset.validation.entity_to_id, dataset.testing.entity_to_id]
+        # print(dataset.training.relation_to_id)
+        # Fetch the value for the entity id by appending Q<entity-id> and querying the wikidata for the value
+        # 
         for value in dataset.relation_to_id.keys():
             word = value.replace('.', ' ').replace('/', ' ').split()
             word = list(set(ps.stem(stem) for stem in word))
             relationship_array.append(word)
+            # Keep another array to track the relationship label
+            relationship_label.append(value)
     if similarity_type=="jaccard":
         jaccard_sim_array = np.zeros(shape=(len(relationship_array), len(relationship_array)))
         for x, relation1 in enumerate(relationship_array):
             for y, relation2 in enumerate(relationship_array):
                 jaccard_sim_array[x][y] = Jaccard_Similarity(relation1, relation2)
 
+        sorted_index = get_pca_ordering(jaccard_sim_array)
+        # send the sentence embeddings and the sorted index for further processing and storing
+        # generate_csv(sorted_index, relationship_label, all_triples, all_entity_ids)
         plt.imshow(jaccard_sim_array, cmap='summer', interpolation='nearest')
         plt.xticks(list(range(len(relationship_array))), relationship_array, rotation='vertical')
         plt.yticks(list(range(len(relationship_array))), relationship_array, rotation='horizontal')
         plt.colorbar(label="Farthest to closest", orientation="vertical")
         plt.title(dataset_name)
         plt.show()
+        
     elif similarity_type=="cosine":
         # Load pretrained model/tokenizer
         # :TODO Use FastTokenizer
@@ -103,8 +146,10 @@ def generate_relationship_sim_matrix(dataset_name, similarity_type="jaccard"):
             last_hidden_states = model(input_ids, attention_mask=attention_mask)
         sentence_features = last_hidden_states[0][:, 0, :].detach().numpy()
         print(sentence_features)
-        array_similarity = squareform(pdist(sentence_features, metric=similarity_type))
+        array_similarity = 1-squareform(pdist(sentence_features, metric=similarity_type))
         # Way to fetch maybe 10-100 items that have a high cosine similarity and look at the 'description-relationship'
+        train, dev, test=build_train_test_dev(array_similarity, threshold=0.25)
+        # save train, dev, test as csv
         svm=sns.heatmap(array_similarity)
         fig=svm.get_figure()
         fig.savefig(f"{dataset_name}_{similarity_type}.png", dpi=400)
@@ -118,10 +163,53 @@ def generate_relationship_sim_matrix(dataset_name, similarity_type="jaccard"):
             plt.annotate('sentence ' + str(i), (pca_sentence_features[i, 0], pca_sentence_features[i, 1]))
         plt.title('2D PCA projection of embedded sentences from BERT')
         plt.show()
-
     else:
         print("This similarity type is not available")
 
+
+def get_pca_ordering(embeddings):
+    # For jaccard, embeddings, perform a 2-d PCA and return
+    pca = PCA(n_components=1)
+    pca.fit(embeddings)
+    pca_sentence_features = pca.transform(embeddings)
+    print(pca_sentence_features)
+    plt.figure(figsize=(2,2))
+    for i in range(len(pca_sentence_features)):
+        plt.scatter(pca_sentence_features[i],0)
+        plt.annotate('sentence ' + str(i), (pca_sentence_features[i],0))
+    plt.title('2D PCA projection of embedded sentences from BERT')
+    plt.show()
+    return sorted(range(len(pca_sentence_features)), key=lambda k: pca_sentence_features[k])
+
+def get_entity_name(value):
+    # Query wikidata, parse and return
+    return
+
+def generate_csv(sorted_index, relationship_label, all_triples, all_entity_ids):
+    # loop through sorted_index
+    # For each index, get the relationship label
+    # get all triples with 'relationship label' from train, validation, text
+    # For corresponding train, validation, test get the respective entity id -> Query Wikidata -> add it to an array
+    # Build this list and save it to a csv
+    # Load csv, split it with 0.8, 0.1, 0.1 split for the new train, validation, test
+    global_triples_list = list()
+    for index in sorted_index:
+        relationship_name = relationship_label[index]
+        all_triples = list()
+        for set_val in all_triples:
+            for value in set_val:
+                # Check if value[1] matches the relationship id, if it does, update the triples with this
+                if value[1] == relationship_name:
+                    # Get the entity description from the triple
+                    all_triples.append([get_entity_name(all_entity_ids[value[0]]), value[1], get_entity_name(all_entity_ids[value[2]])])
+        global_triples_list.extend(all_triples)
+    with open("something.txt", 'w') as f:
+        f.write(str(global_triples_list))
+
+    #:TODO Logic to split it to train, validation, test
+
+    # Values are triples, form a csv for the given name
+    return
 
 def Jaccard_Similarity(words_doc1, words_doc2):
     words_doc1 = set(words_doc1)
@@ -152,4 +240,7 @@ if __name__ == '__main__':
     # main()
     # generate_relationship_sim_matrix("FB15k237", "cosine")
     # generate_relationship_sim_matrix("WN18RR", "cosine")
-    generate_relationship_sim_matrix("Wikidata5M")
+    generate_relationship_sim_matrix("FB15k237", similarity_type="jaccard")
+    # Generate a split based on the similarity
+    # cluster them first? or split them 80-20 and then split the 20 with 10-10 for dev and test
+    # 
