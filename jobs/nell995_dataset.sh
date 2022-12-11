d8fff494936eec43db431f73ca7fcc42738dc0ef
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2022-12-11 17:37:19 +0100

    changes to job file

--- jobs/nell995_dataset.sh
+++ jobs/nell995_dataset.sh
@@ -13,12 +13,14 @@ module purge
 module load 2021
 module load Anaconda3/2021.05
 
-# /home/barath/blogwriter/AutoBlogWriter
 cd $HOME/kg-super-engine/kg-super-engine
 eval "$(conda shell.bash hook)"
 conda activate kg-super
+
 mkdir -p /home/barath/kg-super-engine/kg-super-engine/output/nell995/tfidvectorizer
+
 mkdir -p /home/barath/kg-super-engine/kg-super-engine/output/nell995/sentence
+
 # Build dataset for nell995
 # Cluster with TFID Vectorizer
 python3 relationship-visualizer/data_preprocessing.py --data_path /home/barath/kg-super-engine/kg-super-engine/nell995 --data nell995 --output_dir /home/barath/kg-super-engine/kg-super-engine/output/nell995/tfidvectorizer
