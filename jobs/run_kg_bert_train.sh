7bf595078e0289bcaf854a72b2acbbf23740eb10
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2022-12-11 20:17:18 +0100

    more path related shit

--- jobs/run_kg_bert_train.sh
+++ jobs/run_kg_bert_train.sh
@@ -22,4 +22,4 @@ cp -r /home/barath/kg-super-engine/kg-super-engine/output/fb15k237/sentence/*.ts
 
 # Trigger the relation prediction from the parent folder kg-bert
 
-python3 /home/barath/kg-super-engine/kg-bert/kg-bert/run_bert_relation_prediction.py  --task_name kg --do_train --do_eval --do_predict --data_dir /home/barath/kg-super-engine/kg-bert/kg-bert/FB15k-237 --bert_model bert-base-cased --max_seq_length 25 --train_batch_size 32  --learning_rate 5e-5  --num_train_epochs 20.0  --output_dir /home/barath/kg-super-engine/kg-bert/kg-bert/output_fb15k237/   --gradient_accumulation_steps 1 --eval_batch_size 512
+python3 /home/barath/kg-super-engine/kg-bert/kg-bert/run_bert_relation_prediction.py  --task_name kg --do_train --do_eval --do_predict --data_dir /home/barath/kg-super-engine/kg-bert/kg-bert/data/FB15k-237 --bert_model bert-base-cased --max_seq_length 25 --train_batch_size 32  --learning_rate 5e-5  --num_train_epochs 20.0  --output_dir /home/barath/kg-super-engine/kg-bert/kg-bert/output_fb15k237/   --gradient_accumulation_steps 1 --eval_batch_size 512
