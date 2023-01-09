392e054b7b36dd879d62bad947956ed26009b947
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2023-01-09 13:13:23 +0100

    kg bert changes

--- kgbert/run_bert_relation_prediction.py
+++ kgbert/run_bert_relation_prediction.py
@@ -55,8 +55,6 @@ sweep_configuration = {
      }
 }
 
-sweep_id = wandb.sweep(sweep=sweep_configuration, project="gridsweep")
-
 os.environ['CUDA_VISIBLE_DEVICES']= '0'
 #torch.backends.cudnn.deterministic = True
 
@@ -319,100 +317,7 @@ def compute_metrics(task_name, preds, labels):
 
 
 def main():
-    run = wandb.init(project="gridsweep", entity="barath04")
-    parser = argparse.ArgumentParser()
-
-    ## Required parameters
-    parser.add_argument("--data_dir",
-                        default=None,
-                        type=str,
-                        required=True,
-                        help="The input data dir. Should contain the .tsv files (or other data files) for the task.")
-    parser.add_argument("--bert_model", default=None, type=str, required=True,
-                        help="Bert pre-trained model selected in the list: bert-base-uncased, "
-                        "bert-large-uncased, bert-base-cased, bert-large-cased, bert-base-multilingual-uncased, "
-                        "bert-base-multilingual-cased, bert-base-chinese.")
-    parser.add_argument("--task_name",
-                        default=None,
-                        type=str,
-                        required=True,
-                        help="The name of the task to train.")
-    parser.add_argument("--output_dir",
-                        default=None,
-                        type=str,
-                        required=True,
-                        help="The output directory where the model predictions and checkpoints will be written.")
-
-    ## Other parameters
-    parser.add_argument("--cache_dir",
-                        default="",
-                        type=str,
-                        help="Where do you want to store the pre-trained models downloaded from s3")
-    parser.add_argument("--max_seq_length",
-                        default=128,
-                        type=int,
-                        help="The maximum total input sequence length after WordPiece tokenization. \n"
-                             "Sequences longer than this will be truncated, and sequences shorter \n"
-                             "than this will be padded.")
-    parser.add_argument("--do_train",
-                        action='store_true',
-                        help="Whether to run training.")
-    parser.add_argument("--do_eval",
-                        action='store_true',
-                        help="Whether to run eval on the dev set.")
-    parser.add_argument("--do_predict",
-                        action='store_true',
-                        help="Whether to run eval on the test set.")
-    parser.add_argument("--do_lower_case",
-                        action='store_true',
-                        help="Set this flag if you are using an uncased model.")
-    parser.add_argument("--train_batch_size",
-                        default=32,
-                        type=int,
-                        help="Total batch size for training.")
-    parser.add_argument("--eval_batch_size",
-                        default=8,
-                        type=int,
-                        help="Total batch size for eval.")
-    parser.add_argument("--learning_rate",
-                        default=5e-5,
-                        type=float,
-                        help="The initial learning rate for Adam.")
-    parser.add_argument("--num_train_epochs",
-                        default=3.0,
-                        type=float,
-                        help="Total number of training epochs to perform.")
-    parser.add_argument("--warmup_proportion",
-                        default=0.1,
-                        type=float,
-                        help="Proportion of training to perform linear learning rate warmup for. "
-                             "E.g., 0.1 = 10%% of training.")
-    parser.add_argument("--no_cuda",
-                        action='store_true',
-                        help="Whether not to use CUDA when available")
-    parser.add_argument("--local_rank",
-                        type=int,
-                        default=-1,
-                        help="local_rank for distributed training on gpus")
-    parser.add_argument('--seed',
-                        type=int,
-                        default=42,
-                        help="random seed for initialization")
-    parser.add_argument('--gradient_accumulation_steps',
-                        type=int,
-                        default=1,
-                        help="Number of updates steps to accumulate before performing a backward/update pass.")
-    parser.add_argument('--fp16',
-                        action='store_true',
-                        help="Whether to use 16-bit float precision instead of 32-bit")
-    parser.add_argument('--loss_scale',
-                        type=float, default=0,
-                        help="Loss scaling to improve fp16 numeric stability. Only used when fp16 set to True.\n"
-                             "0 (default value): dynamic loss scaling.\n"
-                             "Positive power of 2: static loss scaling value.\n")
-    parser.add_argument('--server_ip', type=str, default='', help="Can be used for distant debugging.")
-    parser.add_argument('--server_port', type=str, default='', help="Can be used for distant debugging.")
-    args = parser.parse_args()
+    run = wandb.init(project=args.wandb_proj_name, entity="barath04")
     args.learning_rate = wandb.config.lr
 
     if args.server_ip and args.server_port:
@@ -830,7 +735,104 @@ def main():
         'val_acc':hits_at_1
     })
 
+parser = argparse.ArgumentParser()
+
+## Required parameters
+parser.add_argument("--data_dir",
+                    default=None,
+                    type=str,
+                    required=True,
+                    help="The input data dir. Should contain the .tsv files (or other data files) for the task.")
+parser.add_argument("--bert_model", default=None, type=str, required=True,
+                    help="Bert pre-trained model selected in the list: bert-base-uncased, "
+                    "bert-large-uncased, bert-base-cased, bert-large-cased, bert-base-multilingual-uncased, "
+                    "bert-base-multilingual-cased, bert-base-chinese.")
+parser.add_argument("--task_name",
+                    default=None,
+                    type=str,
+                    required=True,
+                    help="The name of the task to train.")
+parser.add_argument("--output_dir",
+                    default=None,
+                    type=str,
+                    required=True,
+                    help="The output directory where the model predictions and checkpoints will be written.")
+
+## Other parameters
+parser.add_argument("--cache_dir",
+                    default="",
+                    type=str,
+                    help="Where do you want to store the pre-trained models downloaded from s3")
+parser.add_argument("--max_seq_length",
+                    default=128,
+                    type=int,
+                    help="The maximum total input sequence length after WordPiece tokenization. \n"
+                            "Sequences longer than this will be truncated, and sequences shorter \n"
+                            "than this will be padded.")
+parser.add_argument("--do_train",
+                    action='store_true',
+                    help="Whether to run training.")
+parser.add_argument("--do_eval",
+                    action='store_true',
+                    help="Whether to run eval on the dev set.")
+parser.add_argument("--do_predict",
+                    action='store_true',
+                    help="Whether to run eval on the test set.")
+parser.add_argument("--do_lower_case",
+                    action='store_true',
+                    help="Set this flag if you are using an uncased model.")
+parser.add_argument("--train_batch_size",
+                    default=32,
+                    type=int,
+                    help="Total batch size for training.")
+parser.add_argument("--eval_batch_size",
+                    default=8,
+                    type=int,
+                    help="Total batch size for eval.")
+parser.add_argument("--learning_rate",
+                    default=5e-5,
+                    type=float,
+                    help="The initial learning rate for Adam.")
+parser.add_argument("--num_train_epochs",
+                    default=3.0,
+                    type=float,
+                    help="Total number of training epochs to perform.")
+parser.add_argument("--warmup_proportion",
+                    default=0.1,
+                    type=float,
+                    help="Proportion of training to perform linear learning rate warmup for. "
+                            "E.g., 0.1 = 10%% of training.")
+parser.add_argument("--no_cuda",
+                    action='store_true',
+                    help="Whether not to use CUDA when available")
+parser.add_argument("--local_rank",
+                    type=int,
+                    default=-1,
+                    help="local_rank for distributed training on gpus")
+parser.add_argument('--seed',
+                    type=int,
+                    default=42,
+                    help="random seed for initialization")
+parser.add_argument('--gradient_accumulation_steps',
+                    type=int,
+                    default=1,
+                    help="Number of updates steps to accumulate before performing a backward/update pass.")
+parser.add_argument('--fp16',
+                    action='store_true',
+                    help="Whether to use 16-bit float precision instead of 32-bit")
+parser.add_argument('--loss_scale',
+                    type=float, default=0,
+                    help="Loss scaling to improve fp16 numeric stability. Only used when fp16 set to True.\n"
+                            "0 (default value): dynamic loss scaling.\n"
+                            "Positive power of 2: static loss scaling value.\n")
+parser.add_argument('--server_ip', type=str, default='', help="Can be used for distant debugging.")
+parser.add_argument('--server_port', type=str, default='', help="Can be used for distant debugging.")
+parser.add_argument('--wandb_proj_name', type=str, default='gridsweep', help="Can be used for distant debugging.")
+args = parser.parse_args()
+
 if __name__ == "__main__":
+    
     # Doing a grid sweep
+    sweep_id = wandb.sweep(sweep=sweep_configuration, project=args.wandb_proj_name)
     wandb.agent(sweep_id, function=main, count=10)
     # main()
