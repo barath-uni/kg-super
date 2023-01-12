76c8fdfb5bdac59f9d49f9ca8186839a71d08db6
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2023-01-12 11:38:25 +0100

    more changes

--- relationship-visualizer/csv_to_tsv.py
+++ relationship-visualizer/csv_to_tsv.py
@@ -3,13 +3,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
 
 centroid = "random_centroid_0"
-output_dir = f"/home/barath/codespace/kg-super/kg-super-engine/output/fb15k237/radial_cluster_sentence/{centroid}"
+output_dir = f"/home/barath/codespace/kg-super/kg-super-engine/output/fb15k237/radial_cluster/{centroid}"
 train_csv = f"{output_dir}/train.csv"
 test_csv = f"{output_dir}/test.csv"
 validation_csv = f"{output_dir}/validation.csv"
@@ -40,9 +40,9 @@ print(df_dev['relationship'].unique())
 print("TEST-----------")
 print(df_test['relationship'].unique())
 
-df.to_csv(f'{output_dir}/train.tsv', sep='\t', encoding='utf-8', index=False, header=None)
-df_test.to_csv(f'{output_dir}/test.tsv', sep='\t', encoding='utf-8', index=False, header=None)
-df_dev.to_csv(f'{output_dir}/dev.tsv', sep='\t', encoding='utf-8', index=False, header=None)
+# df.to_csv(f'{output_dir}/train.tsv', sep='\t', encoding='utf-8', index=False, header=None)
+# df_test.to_csv(f'{output_dir}/test.tsv', sep='\t', encoding='utf-8', index=False, header=None)
+# df_dev.to_csv(f'{output_dir}/dev.tsv', sep='\t', encoding='utf-8', index=False, header=None)
 
 def plot_radial_cluster(df_train, df_test, df_dev):
     relation_dev = df_dev['relationship'].unique()
@@ -74,7 +74,6 @@ def generate_entity_degree_plot(df):
     entity_tail = set(df['tail'].unique())
     
     entities = list(set.union(entity_head, entity_tail))
-    print(len(entities))
     degree_entity_dict = dict()
     # Count Number of times entity appears in Head (out-degree)
     # Count Number of times entity appears in Tail (in-degree)
@@ -84,7 +83,7 @@ def generate_entity_degree_plot(df):
         # in-degree
         in_degree = len(df.loc[df['head'] == entity])
         out_degree = len(df.loc[df['tail'] == entity])
-        total_degree = in_degree+out_degree
+        total_degree = math.log10(in_degree+out_degree)
         if total_degree in degree_entity_dict:
             degree_entity_dict[total_degree].append(entity)
         else:
@@ -96,27 +95,27 @@ def generate_entity_degree_plot(df):
     # Convert the dictionary to {total_degree:number_of_entity}
     for key in degree_entity_dict:
         degree.append(key)
-        number_of_entities.append(len(degree_entity_dict[key]))
+        number_of_entities.append(math.log10(len(degree_entity_dict[key])))
     return degree, number_of_entities
 
 
-# degree_train, no_train = generate_entity_degree_plot(df)
-# degree_dev, no_dev = generate_entity_degree_plot(df_dev)
+degree_train, no_train = generate_entity_degree_plot(df)
+degree_dev, no_dev = generate_entity_degree_plot(df_dev)
 
-# degree_test, no_test = generate_entity_degree_plot(df_test)
-# # Plot these points
+degree_test, no_test = generate_entity_degree_plot(df_test)
+# Plot these points
 
-# fig, ax = plt.subplots()
-# ax.scatter(degree_train, no_train, alpha=0.5, marker="s", label='Train')
-# ax.scatter(degree_dev, no_dev, alpha=0.6, c='g', marker="d", label='Dev')
-# ax.scatter(degree_test, no_test, alpha=0.6, c='r', marker="*", label='Test')
-# ax.set_xlabel(r'Degree', fontsize=15)
-# ax.set_ylabel(r'Number of entities', fontsize=15)
-# ax.set_title('Degree vs Number of entities')
-# ax.grid(True)
-# plt.legend(loc='upper left')
-# fig.tight_layout()
-# plt.savefig(f'{centroid}.png', dpi=fig.dpi)
-# plt.show()
+fig, ax = plt.subplots()
+ax.scatter(degree_train, no_train, alpha=0.5, marker="s", label='Train')
+ax.scatter(degree_dev, no_dev, alpha=0.6, c='g', marker="d", label='Dev')
+ax.scatter(degree_test, no_test, alpha=0.6, c='r', marker="*", label='Test')
+ax.set_xlabel(r'Log of Degree', fontsize=15)
+ax.set_ylabel(r'Log of # entities', fontsize=15)
+ax.set_title('Log Degree vs Number of entities')
+ax.grid(True)
+plt.legend(loc='upper left')
+fig.tight_layout()
+plt.savefig(f'{centroid}.png', dpi=fig.dpi)
+plt.show()
 
 # plot_radial_cluster(df, df_test, df_dev)
