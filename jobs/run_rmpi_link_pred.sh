9ee1446a68df7c2ed77ff6f9291e23bd2a6190e9
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2023-02-03 11:58:35 +0100

    RMPI jobs

---
+++ jobs/run_rmpi_link_pred.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=18
+#SBATCH --job-name=RMPILinkPredTrain
+#SBATCH --time=03:00:00
+#SBATCH --mem=60G
+#SBATCH --output=rmpilinktrain.out
+
+module purge
+module load 2021
+module load Anaconda3/2021.05
+
+# /home/barath/blogwriter/AutoBlogWriter
+cd $HOME/kg-super-engine/kg-super-engine/RMPI
+eval "$(conda shell.bash hook)"
+conda activate kg-super-rmpi
+
+python RMPI/train.py -d nell_v2 -e nell_v2_RMPI_NE --ablation 1
