63063b325887c3310f1947afcc30aa291d9c4419
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2023-03-17 00:10:50 +0100

    s

--- jobs/simkgc/run_simkgc_relation_predictor.sh
+++ jobs/simkgc/run_simkgc_relation_predictor.sh
@@ -19,4 +19,4 @@ eval "$(conda shell.bash hook)"
 conda activate kg-rmpi2
 
 # Not adding a negative sampling until I figure out what it does
-python3 -u main.py --model-dir "$HOME/kg-super-engine/kg-super-engine/simkgc/SimKGC/relationship/model_best.mdl" --pretrained-model bert-base-uncased --pooling mean --lr 1e-5 --use-link-graph --train-path "$HOME/kg-super-engine/kg-super-engine/simkgc/SimKGC/data/FB15k237/train.txt.json" --valid-path "$HOME/kg-super-engine/kg-super-engine/simkgc/SimKGC/data/FB15k237/valid.txt.json" --task FB15k237 --batch-size 512 --print-freq 20 --additive-margin 0.02 --use-amp  --finetune-t --pre-batch 2 --epochs 20 --workers 4 --max-to-keep 5
+python3 -u main.py --model-dir "$HOME/kg-super-engine/kg-super-engine/simkgc/SimKGC/relationship" --pretrained-model bert-base-uncased --pooling mean --lr 1e-5 --use-link-graph --train-path "$HOME/kg-super-engine/kg-super-engine/simkgc/SimKGC/data/FB15k237/train.txt.json" --valid-path "$HOME/kg-super-engine/kg-super-engine/simkgc/SimKGC/data/FB15k237/valid.txt.json" --task FB15k237 --batch-size 512 --print-freq 20 --additive-margin 0.02 --use-amp  --finetune-t --pre-batch 2 --epochs 20 --workers 4 --max-to-keep 5
